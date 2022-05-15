16e69186b1507e112037771476f684d48e2985d2
Jan Waldmann <55444273+janwaldmann@users.noreply.github.com>
2022-05-15 11:27:56 +0200

    Set fixed list of TS parsers

--- neovim/.config/nvim/plugin/nvim-treesitter.lua
+++ neovim/.config/nvim/plugin/nvim-treesitter.lua
@@ -1,5 +1,7 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = 'maintained',
+  ensure_installed = {'bash', 'c', 'cmake', 'cpp', 'cuda', 'go', 'json',
+                      'llvm', 'lua', 'make', 'markdown', 'ninja', 'python',
+                      'rust', 'rst', 'toml', 'vim', 'yaml'},
   highlight = {
     enable = true,
   },
