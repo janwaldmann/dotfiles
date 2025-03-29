d31d01d17e9f5305bd97b67193de5d1837892d90
Jan Waldmann <55444273+janwaldmann@users.noreply.github.com>
2025-03-29 14:08:40 +0100

    Enable telescope history; lazy-load lualine

--- neovim/.config/nvim/lua/plugins/telescope.lua
+++ neovim/.config/nvim/lua/plugins/telescope.lua
@@ -6,12 +6,27 @@ return {
       'nvim-telescope/telescope-fzf-native.nvim',
       build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release'
     },
+    'nvim-telescope/telescope-smart-history.nvim',
+    'kkharji/sqlite.lua'
   },
   config = function()
     local telescope = require('telescope')
+    local telescope_actions = require('telescope.actions')
+    local datapath = vim.fn.stdpath('data')
     telescope.setup {
       defaults = {
         file_ignore_patterns = { '.*%.svg' },
+        history = {
+          ---@diagnostic disable-next-line: param-type-mismatch
+          path = vim.fs.joinpath(datapath, 'telescope_history.sqlite3'),
+          limit = 60
+        },
+        mappings = {
+          i = {
+            ['<C-Down>'] = telescope_actions.cycle_history_next,
+            ['<C-Up>'] = telescope_actions.cycle_history_prev,
+          }
+        }
       },
       extensions = {
         fzf = {
@@ -23,13 +38,13 @@ return {
       }
     }
     telescope.load_extension('fzf')
+    telescope.load_extension('smart_history')
     local opts = { noremap = true }
-    vim.keymap.set('n', '<leader>ff', '<cmd>lua require(\'telescope.builtin\').find_files()<CR>', opts)
-    vim.keymap.set('n', '<leader>fg', '<cmd>lua require(\'telescope.builtin\').live_grep()<CR>', opts)
-    vim.keymap.set('n', '<leader>fb', '<cmd>lua require(\'telescope.builtin\').buffers()<CR>', opts)
-    vim.keymap.set('n', '<leader>fh', '<cmd>lua require(\'telescope.builtin\').help_tags()<CR>', opts)
-    vim.keymap.set('n', '<leader>gs', '<cmd>lua require(\'telescope.builtin\').git_stash()<CR>', opts)
-    vim.keymap.set('n', '<leader>gf', '<cmd>lua require(\'telescope.builtin\').git_files()<CR>', opts)
-    vim.keymap.set('n', '<leader>gb', '<cmd>lua require(\'telescope.builtin\').git_branches()<CR>', opts)
+    local builtin = require('telescope.builtin')
+    vim.keymap.set('n', '<leader>ff', builtin.find_files, opts)
+    vim.keymap.set('n', '<leader>fg', builtin.live_grep, opts)
+    vim.keymap.set('n', '<leader>fb', builtin.buffers, opts)
+    vim.keymap.set('n', '<leader>fh', builtin.help_tags, opts)
+    vim.keymap.set('n', '<leader>gf', builtin.git_files, opts)
   end
 }
