5c187ed15a36bd4854d7f4e45a9523aec9d6dc2d
Jan Waldmann <55444273+janwaldmann@users.noreply.github.com>
2025-05-18 18:03:38 +0200

    Exit trap and some spring cleaning

--- python_install/.local/bin/install_python.sh
+++ python_install/.local/bin/install_python.sh
@@ -1,57 +1,71 @@
 #!/bin/bash
 #
-# Download, build and install a given Python version on Ubuntu.
+# Download, verify, build and install a given Python version.
 # Usage: install_python.sh [PYTHON VERSION]
 #
 # make altinstall is used for installation, which means the binary has a
-# version suffix.
+# version suffix (a typical binary would be /usr/local/bin/pythonX.Y).
 #
+
+set -eo pipefail
 
 err() {
   echo "ERROR: $*" >&2
 }
 
-warn() {
-  echo "WARNING: $*" >&1
-}
-
-install_deps() {
-  echo "Installing dependencies"
+install_dependencies() {
+  echo "Attempting to install dependencies (Ubuntu/Debian)"
+  set +e
   sudo apt-get update -qq
-  sudo apt-get install -qq --assume-yes wget tar build-essential gdb lcov \
-    pkg-config libbz2-dev libffi-dev libgdbm-dev libgdbm-compat-dev liblzma-dev \
-    libncurses5-dev libreadline6-dev libsqlite3-dev libssl-dev \
-    lzma lzma-dev tk-dev uuid-dev zlib1g-dev libdb5.3-dev
+  # See https://github.com/python/cpython/blob/main/.github/workflows/posix-deps-apt.sh
+  sudo apt-get -qq install \
+    build-essential \
+    pkg-config \
+    ccache \
+    gdb \
+    lcov \
+    libb2-dev \
+    libbz2-dev \
+    libffi-dev \
+    libgdbm-dev \
+    libgdbm-compat-dev \
+    liblzma-dev \
+    libncurses5-dev \
+    libreadline6-dev \
+    libsqlite3-dev \
+    libssl-dev \
+    lzma \
+    lzma-dev \
+    strace \
+    tk-dev \
+    uuid-dev \
+    xvfb \
+    zlib1g-dev
   sudo apt-get build-dep -qq python3-defaults
+  set -e
 }
 
-download() {
-  wget "https://www.python.org/ftp/python/${pyversion}/${pyarchive}.tgz" \
-    -O "${tmp_dir}/${pyarchive}.tgz" || exit 1
-  wget "https://www.python.org/ftp/python/${pyversion}/${pyarchive}.tgz.asc" \
+download_and_verify() {
+  wget -nv "https://www.python.org/ftp/python/${pyversion}/${pyarchive}.tgz" \
+    -O "${tmp_dir}/${pyarchive}.tgz"
+  wget -nv "https://www.python.org/ftp/python/${pyversion}/${pyarchive}.tgz.asc" \
     -O "${tmp_dir}/${pyarchive}.tgz.asc"
   if ! gpg --verify "${tmp_dir}/${pyarchive}.tgz.asc"; then
     err "Signature check of downloaded files failed - make sure the GPG keys are imported!"
     err "--> See https://www.python.org/downloads/metadata/pgp/"
-    rm "${tmp_dir:?}/${pyarchive:?}.tgz" && rm "${tmp_dir:?}/${pyarchive:?}.tgz.asc"
     exit 1
   fi
 }
 
 extract() {
-  if [[ -d "${source_dir}" ]]; then
-    warn "Source directory '${source_dir}' already exists; contents will be replaced"
-    sudo rm -rf "${source_dir:?}"
-  fi
   echo "Extracting sources to '${source_dir}'"
   tar -xf "${tmp_dir}/${pyarchive}.tgz" -C "${tmp_dir}"
-  rm "${tmp_dir:?}/${pyarchive:?}.tgz" && rm "${tmp_dir:?}/${pyarchive:?}.tgz.asc"
 }
 
 build() {
-  cd "${source_dir}" || exit 1
+  cd "${source_dir}"
   echo "Configuring"
-  ./configure --enable-optimizations
+  ./configure --enable-optimizations --with-lto=yes
   echo "Running make"
   make -j 4
   echo "Running make altinstall"
@@ -59,8 +73,10 @@ build() {
 }
 
 clean_up() {
-  echo "Removing source directory '${source_dir}'"
-  sudo rm -rf "${source_dir:?}"
+  if [[ -d "${tmp_dir}" ]]; then
+    echo "Deleting temporary directory '${tmp_dir}'"
+    sudo rm -rf "${tmp_dir:?}"
+  fi
 }
 
 main() {
@@ -69,25 +85,19 @@ main() {
     exit 1
   fi
   readonly pyversion="$1"
-  if [[ -z "${TMPDIR}" ]]; then
-    readonly tmp_dir="/tmp"
-  else
-    readonly tmp_dir="${TMPDIR}"
-  fi
-  echo "Using '${tmp_dir}' for temporary files"
-  if [[ ! -d "${tmp_dir}" ]]; then
-    err "Temporary directory '${tmp_dir}' does not exist"
-    exit 1
-  fi
   readonly pyarchive="Python-${pyversion}"
-  readonly source_dir="${tmp_dir}/${pyarchive}"
+  readonly tmp_dir=$(mktemp -d -t ${pyarchive}-install.XXXXXXXXXX)
+  readonly source_dir="${tmp_dir:?}/${pyarchive}"
+  echo "Using temporary directory '${tmp_dir}'"
 
-  download
-  install_deps
+  set -u
+
+  download_and_verify
+  install_dependencies
   extract
   build
-  clean_up
-  echo "Done"
+  echo "Successfully installed Python ${pyversion}"
 }
 
+trap clean_up EXIT
 main "$@"
